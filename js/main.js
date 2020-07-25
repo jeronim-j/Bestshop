79cbba9d78264016fbf0e566ad5dc4d8f493c069
jeronim-j <jeronim.j@o2.pl>
2020-07-25 14:24:46 +0200

    Added Calculator functionality structure and style

---
+++ js/main.js
@@ -0,0 +1,16 @@
+//Hamburger menu script
+document.addEventListener("DOMContentLoaded", function() {
+    const navMenu = document.querySelector(".navMenu");
+    const hamburger = document.querySelector(".hamburger");
+
+    hamburger.addEventListener("click", function() {
+        hamburger.classList.toggle("is-active");
+        navMenu.classList.toggle("show");
+
+});
+
+});
+
+//Calculator Script
+
+
